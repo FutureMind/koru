cd4270103904cf28cf28f8d2c2114ee913df0218
Michal Klimczak <m.klimczak@futuremind.com>
2021-01-27 12:00:43 +0100

    #7 Fix wrapping functions with Unit return type

--- ioswrapper-processor/src/jvmTest/kotlin/com/futuremind/iossuspendwrapper/processor/WrappedClassCodegenTest.kt
+++ ioswrapper-processor/src/jvmTest/kotlin/com/futuremind/iossuspendwrapper/processor/WrappedClassCodegenTest.kt
@@ -3,6 +3,7 @@ package com.futuremind.iossuspendwrapper.processor
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
@@ -38,7 +39,6 @@ class WrappedClassCodegenTest {
 
         compilationResult.exitCode shouldBe KotlinCompilation.ExitCode.COMPILATION_ERROR
         compilationResult.messages shouldContain "ExportedScopeProvider can only be applied to a class extending ScopeProvider interface"
-
 
     }
 
@@ -76,6 +76,63 @@ class WrappedClassCodegenTest {
                     |public val exportedScopeProvider_mainScopeProvider: MainScopeProvider = MainScopeProvider()
                 """.trimMargin().trim()
 
+    }
+
+    @Test
+    fun `should generate wrapper for a suspend function returning Unit`() {
+
+        val source = SourceFile.kotlin(
+            "unit1.kt",
+            """
+                package com.futuremind.kmm101.test
+                
+                import com.futuremind.iossuspendwrapper.WrapForIos
+                
+                @WrapForIos
+                class FireAndWait {
+                    suspend fun doSthSuspending(whatever: Int){ }
+                }
+            """
+        )
+
+        val compilationResult = prepareCompilation(source).compile()
+
+        val generatedClass = compilationResult.classLoader
+            .loadClass("com.futuremind.kmm101.test.FireAndWaitIos")
+
+        compilationResult.exitCode shouldBe KotlinCompilation.ExitCode.OK
+        with(generatedClass.kotlin.members.find { it.name == "doSthSuspending" }!!){
+            returnType.toString() shouldBe "com.futuremind.iossuspendwrapper.SuspendWrapper<kotlin.Unit>"
+        }
+    }
+
+    @Test
+    fun `should generate wrapper for a blocking function returning Unit`() {
+
+        val source = SourceFile.kotlin(
+            "unit2.kt",
+            """
+                package com.futuremind.kmm101.test
+                
+                import com.futuremind.iossuspendwrapper.WrapForIos
+                
+                @WrapForIos
+                class FireAndWait {
+                    fun doSthBlocking(whatever: Int){ }
+                }
+            """
+        )
+
+        val compilationResult = prepareCompilation(source).compile()
+
+        val generatedClass = compilationResult.classLoader
+            .loadClass("com.futuremind.kmm101.test.FireAndWaitIos")
+
+        compilationResult.exitCode shouldBe KotlinCompilation.ExitCode.OK
+
+        with(generatedClass.kotlin.members.find { it.name == "doSthBlocking" }!!){
+            returnType.toString() shouldBe "kotlin.Unit"
+        }
     }
 
     private fun prepareCompilation(vararg sourceFiles: SourceFile) = KotlinCompilation()
