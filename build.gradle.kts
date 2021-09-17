dcbd5519aa74fd8c30f37652c4b2dcf1066dfe62
Michal Klimczak <m.klimczak@futuremind.com>
2021-09-17 10:16:54 +0200

    #40: generate apple silicon targets
    Update kotlin to 1.5.30, coroutines to 1.5.2 and provide apple targets including the new ones for apple silicon.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+        classpath("com.android.tools.build:gradle:7.0.2")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.0.0")
     }
 }
--- koru-processor/build.gradle.kts
+++ koru-processor/build.gradle.kts
@@ -32,13 +32,13 @@ kotlin {
                 implementation(project(":koru"))
 
                 //code generation
-                val kotlinpoetVersion = "1.8.0"
+                val kotlinpoetVersion = "1.9.0"
                 implementation("com.squareup:kotlinpoet:$kotlinpoetVersion")
                 implementation("com.squareup:kotlinpoet-metadata:$kotlinpoetVersion")
                 implementation("com.squareup:kotlinpoet-metadata-specs:$kotlinpoetVersion")
                 implementation("com.squareup:kotlinpoet-classinspector-elements:$kotlinpoetVersion")
 
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-native-mt")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
             }
         }
 
