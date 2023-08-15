7a05ff71fd492d321327f0b72bcf98c64205bbb4
marty-suzuki <s1180183@gmail.com>
2023-08-15 23:07:25 +0900

    fix CompilerPlugin

--- koru-compiler-plugin/src/main/kotlin/com/futuremind/koru/gradle/CompilerPlugin.kt
+++ koru-compiler-plugin/src/main/kotlin/com/futuremind/koru/gradle/CompilerPlugin.kt
@@ -4,21 +4,14 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import java.io.File
 
 
 class CompilerPlugin : Plugin<Project> {
 
     override fun apply(project: Project) = with(project) {
-        val extension: KoruPluginExtension = extensions.create("koru")
         requireKspPluginDependency()
-        enableKspRunForCommonMainSourceSet()
-        makeSureCompilationIsRunAfterKsp()
         afterEvaluate {
-            requireSourceSetsNamesSet(extension.nativeSourceSetNames)
-            addGeneratedFilesToSourceSets(extension.nativeSourceSetNames)
+            enableKspRunForAppleSourceSet()
         }
     }
 
@@ -28,45 +21,13 @@ class CompilerPlugin : Plugin<Project> {
         }
     }
 
-    private fun Project.enableKspRunForCommonMainSourceSet() = dependencies {
-        //todo don't hardcode version
-        add("kspCommonMainMetadata", "com.futuremind:koru-processor:0.12.0")
-    }
-
-    private fun Project.makeSureCompilationIsRunAfterKsp() = tasks
-        .matching {
-            it.name.startsWith("compileKotlinIos")
-                    || it.name.startsWith("compileKotlinMacos")
-                    || it.name.startsWith("compileKotlinWatchos")
-                    || it.name.startsWith("compileKotlinTvos")
-        }
-        .configureEach {
-            dependsOn("kspCommonMainKotlinMetadata")
-        }
-
-    private fun requireSourceSetsNamesSet(nativeSourceSetNames: List<String>) {
-        require(nativeSourceSetNames.isNotEmpty()) {
-            "You need to provide the name of your main native source set in your build.gradle, e.g. koru.nativeSourceSetNames = listOf(\"iosMain\")"
-        }
-    }
-
-    private fun Project.addGeneratedFilesToSourceSets(sourceSetNames: List<String>) {
-        val anyMatch = extensions
-            .getByType<KotlinMultiplatformExtension>().sourceSets
-            .any { sourceSetNames.contains(it.name) }
-        if (!anyMatch) throw IllegalStateException("None of the provided source set names were matched: $sourceSetNames. You need to provide the name of your main native source set in your build.gradle, e.g. koru.nativeSourceSetNames = listOf(\"iosMain\")")
-
-        extensions
-            .getByType<KotlinMultiplatformExtension>().sourceSets
-            .matching { sourceSetNames.contains(it.name) }
-            .configureEach {
-                kotlin.srcDir("${project.buildDir.absolutePath}${File.separator}generated${File.separator}ksp${File.separator}metadata${File.separator}commonMain${File.separator}kotlin")
+    private fun Project.enableKspRunForAppleSourceSet() = dependencies {
+        val kspAppleRegex = "ksp[Ios|Tvos|Macos|Watchos]+(?!.*Test$)".toRegex()
+        configurations
+            .filter { it.name.contains(kspAppleRegex) }
+            .forEach {
+                //todo don't hardcode version
+                add(it.name, "com.futuremind:koru-processor:0.13.0")
             }
     }
 }
-
-open class KoruPluginExtension {
-    var nativeSourceSetNames: List<String> = listOf()
-}
-
-
